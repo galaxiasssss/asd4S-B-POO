e84ef3a9077f813c6f44d48975b9f8b53667be17
unknown <vitororbe@gmail.com>
2025-08-21 22:10:15 -0300

    Projeto Criado

---
+++ suzuki/src/main/java/com/aanw/suzuki/SuzukiApplication.java
@@ -0,0 +1,13 @@
+package com.aanw.suzuki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuzukiApplication.class, args);
+	}
+
+}
---
+++ suzuki/src/test/java/com/aanw/suzuki/SuzukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aanw.suzuki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuzukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
